0df64d65abf06cc3622c9ffd7995148d01b1069a
Richard Miskin <r.p.miskin@gmail.com>
2018-01-30 20:44:00 +0000

    Work in progress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import {List, ListItem} from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
 import './App.css';
+import VennDiagram from './VennDiagram';
+
+function nextLetter(terms) {
+    let code = 65;
+    let letter = '"'+String.fromCharCode(code)+'"';
+    while (terms.indexOf(letter) > -1) {
+      code++; 
+      letter = '"'+String.fromCharCode(code)+'"';
+    }
+    return letter;
+    
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {terms:['"A"','"B"', '"D"']};
+    this.renderList = this.renderList.bind(this);
+  }
+
+  renderList(terms) {
+      return terms.map((t,index) => 
+              <ListItem
+                  key={t}
+                  primaryText={t}
+                  rightIconButton={
+                      <IconButton
+                          tooltip='Delete'
+                          onClick={()=> {
+                              // Copy the existing terms and remove the
+                              // clicked on item
+                              const newTerms = [...this.state.terms];
+                              newTerms.splice(index, 1);
+                              this.setState({terms:newTerms});
+                          } }
+                      >
+                        <ActionDelete />
+                      </IconButton>
+                  }
+              />);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <AppBar title="Query Builder"/>
+          <div style={{display: 'inline-block'}}>
+            <div className='controls'>
+              <List className='list'>
+                <div className='list-heading'>
+                  <div className='title'><h2>Search Terms</h2></div>
+                  <FloatingActionButton 
+                      className='add-button'
+                      secondary={true}
+                      onClick={()=> {
+                          const newTerms = this.state.terms;
+                          newTerms.push(nextLetter(newTerms));
+                          this.setState({terms:newTerms});
+                      }}>
+                    <ContentAdd />
+                  </FloatingActionButton>
+                </div>
+                {this.renderList(this.state.terms)}
+              </List>
+            </div>
+            <div className='diagram'>
+              <VennDiagram terms={this.state.terms} size={[500,500]}/>
+            </div>
+          </div>
+          <div>
+            <FlatButton label='Perform search' fullWidth={true}/>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/VennDiagram.js
@@ -0,0 +1,53 @@
+/* global venn */
+/* global d3 */
+import React, { Component } from 'react';
+import './App.css';
+
+function mapTermsToSets(terms) {
+    const defaultSize = 100;
+    const sets = [];
+    for(let i = 0; i < terms.length; i+=1) {
+       sets.push({sets:[terms[i]], size: defaultSize}); 
+        for(let j = i+1; j < terms.length; j+=1) {
+           sets.push({sets:[terms[i], terms[j]], size: defaultSize/2}); 
+        }
+    }
+    return sets;
+}
+
+class VennDiagram extends Component {
+    constructor(props) {
+        super(props);
+        this.createVennDiagram = this.createVennDiagram.bind(this);
+    }
+
+    componentDidMount() {
+        this.createVennDiagram();
+    }
+
+    componentDidUpdate() {
+        this.createVennDiagram();
+    }
+
+    createVennDiagram() {
+        console.log(JSON.stringify(this.props));
+
+        var sets = mapTermsToSets(this.props.terms || []);
+                    
+        var chart = venn.VennDiagram();
+        d3.select(this.node)
+            .datum(sets)
+            .call(chart);
+
+        d3.select(this.node).selectAll('.venn-circle path').style('fill-opacity', 0.4).style('stroke-width', 4).style('stroke', 'black');
+        d3.select(this.node).selectAll('text').style('fill', 'white');
+    d3.select(this.node).selectAll('.venn-area').on('mouseover', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
+    d3.select(this.node).selectAll('.venn-area').on('click', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
+    }
+
+    render() {
+        return <div ref={node=>this.node=node} ></div>;
+    }
+}
+
+export default VennDiagram;
