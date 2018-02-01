5b3826009dfa6c943e2490b2c1c13542c81eeb06
Richard Miskin <r.p.miskin@gmail.com>
2018-02-01 20:53:51 +0000

    Added prettier

--- src/App.js
+++ src/App.js
@@ -4,83 +4,87 @@ import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from 'material-ui/List';
 import IconButton from 'material-ui/IconButton';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 import './App.css';
 import VennDiagram from './VennDiagram';
 
 function nextLetter(terms) {
-    let code = 65;
-    let letter = '"'+String.fromCharCode(code)+'"';
-    while (terms.indexOf(letter) > -1) {
-      code++; 
-      letter = '"'+String.fromCharCode(code)+'"';
-    }
-    return letter;
-    
+  let code = 65;
+  let letter = '"' + String.fromCharCode(code) + '"';
+  while (terms.indexOf(letter) > -1) {
+    code++;
+    letter = '"' + String.fromCharCode(code) + '"';
+  }
+
+  return letter;
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {terms:['"A"','"B"', '"D"']};
+    this.state = { terms: ['"A"', '"B"', '"D"'] };
     this.renderList = this.renderList.bind(this);
   }
 
   renderList(terms) {
-      return terms.map((t,index) => 
-              <ListItem
-                  key={t}
-                  primaryText={t}
-                  rightIconButton={
-                      <IconButton
-                          tooltip='Delete'
-                          onClick={()=> {
-                              // Copy the existing terms and remove the
-                              // clicked on item
-                              const newTerms = [...this.state.terms];
-                              newTerms.splice(index, 1);
-			      console.log(newTerms);
-                              this.setState({terms:newTerms});
-                          } }
-                      >
-                        <ActionDelete />
-                      </IconButton>
-                  }
-              />);
+    return terms.map((t, index) => (
+      <ListItem
+        key={t}
+        primaryText={t}
+        rightIconButton={
+          <IconButton
+            tooltip="Delete"
+            onClick={() => {
+              // Copy the existing terms and remove the
+              // clicked on item
+              const newTerms = [...this.state.terms];
+              newTerms.splice(index, 1);
+              console.log(newTerms);
+              this.setState({ terms: newTerms });
+            }}
+          >
+            <ActionDelete />
+          </IconButton>
+        }
+      />
+    ));
   }
 
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <AppBar title="Query Builder"/>
-          <div className='body'>
-            <div className='controls'>
-              <List className='list'>
-                <div className='list-heading'>
-                  <div className='title'><h2>Search Terms</h2></div>
-                  <FloatingActionButton 
-                      className='add-button'
-                      secondary={true}
-                      onClick={()=> {
-                          const newTerms = this.state.terms;
-                          newTerms.push(nextLetter(newTerms));
-                          this.setState({terms:newTerms});
-                      }}>
+          <AppBar title="Query Builder" />
+          <div className="body">
+            <div className="controls">
+              <List className="list">
+                <div className="list-heading">
+                  <div className="title">
+                    <h2>Search Terms</h2>
+                  </div>
+                  <FloatingActionButton
+                    className="add-button"
+                    secondary={true}
+                    onClick={() => {
+                      const newTerms = this.state.terms;
+                      newTerms.push(nextLetter(newTerms));
+                      this.setState({ terms: newTerms });
+                    }}
+                  >
                     <ContentAdd />
                   </FloatingActionButton>
                 </div>
                 {this.renderList(this.state.terms)}
               </List>
             </div>
-            <div className='diagram'>
-              <VennDiagram terms={this.state.terms} size={[500,500]}/>
+            <div className="diagram">
+              <VennDiagram terms={this.state.terms} size={[500, 500]} />
             </div>
           </div>
           <div>
-            <FlatButton label='Perform search' fullWidth={true}/>
+            <FlatButton label="Perform search" fullWidth={true} />
           </div>
         </div>
       </MuiThemeProvider>
--- src/VennDiagram.js
+++ src/VennDiagram.js
@@ -4,72 +4,75 @@ import React, { Component } from 'react';
 import padleft from 'lodash.padleft';
 import './App.css';
 
-
 // For an array of terms that are (2^n)-1 combinations
 // available. One way of working these out is to count from
 // 1 to 2^n, converting to binary and choosing any element
 // where  the binary is '1'.
 function mapTermsToSets(terms) {
-    const sets = [];
-    const combinations = Math.pow(2, terms.length);
-    for (let i = 1; i < combinations; i++) {
-        const binaryString = padleft(i.toString(2), terms.length, '0');
-        console.log(`${i}_${binaryString}`);
-        const currentSet =[];
-        for (let j = binaryString.length -1; j > -1; j--) {
-            if (binaryString[j]==='1') {
-                currentSet.push(terms[j]);
-            }
-        }
-        let size = Math.max(10, Math.round(100/currentSet.length));
-        sets.push({sets: currentSet, size});
+  const sets = [];
+  const combinations = Math.pow(2, terms.length);
+  for (let i = 1; i < combinations; i++) {
+    const binaryString = padleft(i.toString(2), terms.length, '0');
+    console.log(`${i}_${binaryString}`);
+    const currentSet = [];
+    for (let j = binaryString.length - 1; j > -1; j--) {
+      if (binaryString[j] === '1') {
+        currentSet.push(terms[j]);
+      }
     }
-    return sets;
+    let size = Math.max(10, Math.round(100 / currentSet.length));
+    sets.push({ sets: currentSet, size });
+  }
+  return sets;
 }
 
 class VennDiagram extends Component {
-    constructor(props) {
-        super(props);
-        this.createVennDiagram = this.createVennDiagram.bind(this);
+  constructor(props) {
+    super(props);
+    this.createVennDiagram = this.createVennDiagram.bind(this);
+  }
+
+  componentDidMount() {
+    this.createVennDiagram();
+  }
+
+  componentDidUpdate() {
+    this.createVennDiagram();
+  }
+
+  createVennDiagram() {
+    console.log(JSON.stringify(this.props));
+
+    var sets = mapTermsToSets(this.props.terms || []);
+    console.log(sets);
+    if (!sets || sets.length === 0) {
+      // TODO The Venn diagram layout code does not like  empty sets!
+      return;
     }
-
-    componentDidMount() {
-        this.createVennDiagram();
+    if (!this.chart) {
+      this.chart = venn.VennDiagram();
     }
+    d3
+      .select(this.node)
+      .datum(sets)
+      .call(this.chart);
 
-    componentDidUpdate() {
-        this.createVennDiagram();
-    }
+    //        d3.select(this.node).selectAll('.venn-circle path').style('fill-opacity', 0.4).style('stroke-width', 4).style('stroke', 'black');
+    //       d3.select(this.node).selectAll('text').style('fill', 'white');
+    //   d3.select(this.node).selectAll('.venn-area').on('mouseover', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
+    //   d3.select(this.node).selectAll('.venn-area').on('click', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
 
-    createVennDiagram() {
-        console.log(JSON.stringify(this.props));
+    d3
+      .select(this.node)
+      .selectAll('g path')
+      .on('mouseup', function(d, i) {
+        console.log(JSON.stringify(d));
+      });
+  }
 
-        var sets = mapTermsToSets(this.props.terms || []);
-	console.log(sets);
-	if (!sets || sets.length === 0) {
-	  // TODO The Venn diagram layout code does not like  empty sets!
-          return;
-	}
-        if (!this.chart){
-          this.chart = venn.VennDiagram();
-        }
-        d3.select(this.node)
-            .datum(sets)
-            .call(this.chart);
-
-//        d3.select(this.node).selectAll('.venn-circle path').style('fill-opacity', 0.4).style('stroke-width', 4).style('stroke', 'black');
- //       d3.select(this.node).selectAll('text').style('fill', 'white');
- //   d3.select(this.node).selectAll('.venn-area').on('mouseover', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
- //   d3.select(this.node).selectAll('.venn-area').on('click', (d,i)=>console.log(`${JSON.stringify(d)} - ${JSON.stringify(i)}`));
-
-        d3.select(this.node)
-          .selectAll('g path')
-          .on('mouseup', function(d, i) {console.log(JSON.stringify(d));});
-    }
-
-    render() {
-        return <div ref={node=>this.node=node} ></div>;
-    }
+  render() {
+    return <div ref={node => (this.node = node)} />;
+  }
 }
 
 export default VennDiagram;
